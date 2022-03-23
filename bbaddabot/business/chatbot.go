b622c4eed38d1927a3f29546139423e6214433d2
phj1120 <hyeonj1998@naver.com>
2022-03-23 22:04:21 +0900

    시간 표기 변경

--- bbaddabot/business/chatbot.go
+++ bbaddabot/business/chatbot.go
@@ -22,24 +22,39 @@ func Chatbot(s *discordgo.Session, m *discordgo.MessageCreate) {
 	var subMsg string
 	if request == "!기록" {
 		userList, _ := ps.SelectUserList(m.GuildID)
+
 		msg = fmt.Sprintf("[%s]\n", time.Now().Format("20060102 15:04"))
 		for _, user = range userList {
 			bbadda, _ = ps.SelectBbadda(user.UserNum)
 			studyTime, _ = ps.SelectStudyTotalTodayByUserNum(user.UserNum)
-			// subMsg = fmt.Sprintf("[%s] %s : 총 %d 분 / %d 개\n", time.Now().Format("20060102 15:04"), user.UserName, studyTime, bbadda)
-			subMsg = fmt.Sprintf("%s : 총 %d 분 / %d 개\n", user.UserName, studyTime, bbadda)
+			subMsg = fmt.Sprintf("%s : 총 %s / %d 개\n", user.UserName, minuteToHour(studyTime), bbadda)
 			msg += subMsg
 		}
-		s.ChannelMessageSend(m.ChannelID, msg)
 	}
 
 	if request == "!빠따" {
 		msg = fmt.Sprintf("[%s] %s : 총 %d 개", time.Now().Format("20060102 15:04"), user.UserName, bbadda)
-		s.ChannelMessageSend(m.ChannelID, msg)
 	}
 
 	if request == "!공부시간" {
-		msg = fmt.Sprintf("[%s] %s : 총 %d 분", time.Now().Format("20060102 15:04"), user.UserName, studyTime)
-		s.ChannelMessageSend(m.ChannelID, msg)
+		msg = fmt.Sprintf("[%s] %s : 총 %s", time.Now().Format("20060102 15:04"), user.UserName, minuteToHour(studyTime))
 	}
+
+	s.ChannelMessageSend(m.ChannelID, msg)
+
+}
+
+func minuteToHour(minute int) string {
+	var m int
+	var h int
+	var msg string
+
+	if minute >= 60 {
+		h = minute / 60
+		m = minute - h*60
+		msg = fmt.Sprintf("%d 시간 %d 분", h, m)
+	} else {
+		msg = fmt.Sprintf("%d 분", minute)
+	}
+	return msg
 }
