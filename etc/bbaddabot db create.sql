392c9b39d98d202a1937c421725bd27d1f5aeb89
phj1120 <hyeonj1998@naver.com>
2022-06-09 23:51:33 +0900

    Planning Change Logic

--- etc/bbaddabot db create.sql
+++ etc/bbaddabot db create.sql
@@ -1,6 +1,11 @@
-create database bbaddabot;
+# 디비 생성
+create database lsa_test;
 
-create user 'bbadda'@'%' identified by 'mybbadda';
-grant all on bbaddabot.* to 'bbadda'@'%';
+# 사용자 생성
+create user 'lsa'@'%' identified by 'lsa_pass';
 
-show grants for 'bbadda'@'%';
+# 사용자에게 DB의 모든 테이블을 all 제어할 수 있는 권한 부여
+grant all on lsa_test.* to 'lsa'@'%';
+
+
+show grants for 'lsa'@'%';
--- etc/oftenQuery.sql
+++ etc/oftenQuery.sql
@@ -1,5 +1,6 @@
 use bbaddabot;
 
+# 단순 조회
 ## 테이블 기본 조회
 SELECT * FROM bbaddabot.channel;
 
@@ -17,6 +18,8 @@ SELECT * FROM bbaddabot.history where date(time) = date(now()) and userNum = 13;
 ## 총 데이터 조회
 SELECT COUNT(*) FROM bbaddabot.history;
 
+
+# Study Total 조회
 ## 개인별 일일 공부 시간 조회
 SELECT * FROM bbaddabot.studyTotal
 WHERE userNum = (SELECT userNum FROM bbaddabot.user WHERE userName="박현준" AND guildId = "951671348298661938")
@@ -26,9 +29,54 @@ SELECT * FROM bbaddabot.studyTotal
 WHERE userNum = (SELECT userNum FROM bbaddabot.user WHERE userName="유민상" AND guildId = "951671348298661938")
 AND date='2022-04-11';
 
+## 주간 공부 현황 
+SELECT
+	date_format(date,'%Y-%m-%d') AS 일자, userName AS 이름, studyTime AS 공부시간
+FROM
+    studyTotal LEFT JOIN user ON studyTotal.userNum = user.userNum
+WHERE
+    date_format(date,'%Y-%m-%d')
+    BETWEEN
+        (SELECT ADDDATE(CURDATE(), - WEEKDAY(CURDATE()) + 0 ))
+    AND
+        (SELECT ADDDATE(CURDATE(), - WEEKDAY(CURDATE()) + 6 ));
 
-SELECT * FROM bbaddabot.studyTotal
-WHERE date='2022-04-22';
+## 이번 달
+SELECT
+	date_format(date,'%Y-%m-%d') AS 일자, userName AS 이름, studyTime AS 공부시간
+FROM
+    studyTotal LEFT JOIN user ON studyTotal.userNum = user.userNum
+WHERE
+    date_format(date,'%Y-%m-%d')
+    BETWEEN
+        (SELECT DATE_FORMAT(NOW(), '%x-%m-01'))
+    AND
+        (SELECT LAST_DAY(NOW()));
+
+# 저번 달 ( 하드코딩 ) ------------------------------------------------------------------------------------------ #
+SELECT
+	date_format(date,'%Y-%m-%d') AS 일자, userName AS 이름, studyTime AS 공부시간
+FROM
+    studyTotal LEFT JOIN user ON studyTotal.userNum = user.userNum
+WHERE
+    date_format(date,'%Y-%m-%d')
+    BETWEEN
+        (SELECT DATE_FORMAT(NOW(), '2022-04-01'))
+    AND
+        (SELECT DATE_FORMAT(NOW(), '2022-04-30'));
+        
+SELECT
+	userName, sum(studyTime) as 공부시간
+FROM
+    studyTotal LEFT JOIN user ON studyTotal.userNum = user.userNum
+WHERE
+    date_format(date,'%Y-%m-%d')
+    BETWEEN
+        (SELECT DATE_FORMAT(NOW(), '2022-04-01'))
+    AND
+        (SELECT DATE_FORMAT(NOW(), '2022-04-30'))
+group by userName;
+# ----------------------------------------------------------------------------------------------------------- #
 
 ## 길드 아이디, 유저 아이디로 오늘 (0 ~ 24) 공부한 시간 조회
 select studytime from bbaddabot.studyTotal
@@ -36,55 +84,7 @@ WHERE date =DATE(NOW()) and
 usernum = (select usernum from user 
     where userid='759364130569584640' and guildid = '951671348298661938');
 
-## 06시 기준으로 조회 하도록 변경 할 것
-
-## 빠다 초기화
-update user set bbadda=0;
-
-## 주간 통계 / 원하는 값 X, 그래도 나중에 할때 도움될 것 같아 기록
-## https://deeplify.dev/database/troubleshoot/mysql-daily-weekly-monthly
-SELECT 
-userNum,
-DATE_FORMAT(DATE_SUB(`date`, INTERVAL (DAYOFWEEK(`date`)-1) DAY), '%Y/%m/%d') as start, 
-DATE_FORMAT(DATE_SUB(`date`, INTERVAL (DAYOFWEEK(`date`)-7) DAY), '%Y/%m/%d') as end, 
-DATE_FORMAT(`date`, '%Y%U') AS `date`, sum(`studyTime`)  as weekStudy
-FROM bbaddabot.studyTotal GROUP BY userNum;
-
-select version();
-show status;
-show status like '%thread%';
-
--- 유저별 이번주 공부 시간 구하기
--- https://suzxc2468.tistory.com/145
-SELECT
-    userNum, sum(studyTime)
-FROM
-    studyTotal
-WHERE
-    date_format(date,'%Y-%m-%d')
-    BETWEEN
-        (SELECT ADDDATE(CURDATE(), - WEEKDAY(CURDATE()) + 0 ))
-    AND
-        (SELECT ADDDATE(CURDATE(), - WEEKDAY(CURDATE()) + 6 ))
-group by userNum;
-
--- 이번달 공부 시간 구하기
--- https://blog.naver.com/PostView.nhn?isHttpsRedirect=true&blogId=classe82&logNo=20149848301&redirect=Dlog&widgetTypeCall=true
-SELECT
-    userNum, sum(studyTime)
-FROM
-    studyTotal
-WHERE
-    date_format(date,'%Y-%m-%d')
-    BETWEEN
-        (SELECT LAST_DAY(NOW() - interval 1 month))
-    AND
-        (SELECT LAST_DAY(NOW()))
-group by userNum;
-
-
-
-# 유저 한 명
+## 유저별 이번주 공부 시간
 SELECT
     userNum, sum(studyTime)
 FROM
@@ -97,7 +97,8 @@ WHERE
         (SELECT ADDDATE(CURDATE(), - WEEKDAY(CURDATE()) + 6 ))
 	AND
 		userNum = '1';
-        
+       
+## 유저별 이번달 공부 시간
 SELECT
     sum(studyTime)
 FROM
@@ -110,3 +111,8 @@ WHERE
         (SELECT LAST_DAY(NOW()))
 	AND userNum = '1';
 
+
+# DB 운영
+select version();
+show status;
+show status like '%thread%';
